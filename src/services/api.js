54105fd7053603711195d631c8757bb86423f38f
SuryaSubramaniyam <153812383+SuryaSubramaniyam@users.noreply.github.com>
2026-01-07 16:34:03 +0530

    Configure production API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,19 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api", 
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-  return req;
-});
+
+API.interceptors.request.use(
+  (req) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      req.headers.Authorization = `Bearer ${token}`;
+    }
+    return req;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default API;
